029dd7fd81a90c7a337717389bf8bccc36353198
Himanshu <him_bhar@hotmail.com>
2025-03-29 10:51:58 +0000

    Dynamic property Source defined in test

--- src/test/java/com/bookcatalog/dao/BookDaoTest.java
+++ src/test/java/com/bookcatalog/dao/BookDaoTest.java
@@ -10,12 +10,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest(classes = { BookCatalogApplication.class })
 //Comment next 2 lines to disable testcontainers and use h2 instead
 @Import(TestcontainersConfiguration.class)
-@TestPropertySource(properties = { "spring.jooq.sql-dialect=POSTGRES" })
+//@TestPropertySource(properties = { "spring.jooq.sql-dialect=POSTGRES" })
+//Above line and dynamicpropertysource does the same thing, so comment that instead, above is another style
 public class BookDaoTest {
   @Autowired
   private BookDao bookDao;
@@ -25,5 +31,10 @@ public class BookDaoTest {
     Book book = Book.builder().title("test-title").description("a sample for test").build();
     Long bookId = bookDao.createBook(book);
     Assertions.assertAll(() -> MatcherAssert.assertThat(bookId, Matchers.notNullValue(Long.class)));
+  }
+
+  @DynamicPropertySource
+  static void registerPostgres(DynamicPropertyRegistry registry) {
+    registry.add("spring.jooq.sql-dialect", () -> "POSTGRES");
   }
 }
